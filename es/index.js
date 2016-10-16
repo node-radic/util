38d57abc453b5d125b933910ac9cd376faa055e5
Robin Radic <robin@radic.nl>
2016-10-16 23:22:01 +0200

    after remove ignore files

--- es/index.js
+++
@@ -1,7 +0,0 @@
-export * from './config';
-export * from './functions';
-export * from './general';
-export * from './JSON';
-export * from './material';
-export * from './object';
-export * from './storage';
