423b4e18225da5731bf783acd0b99e2500b5b5ba
Robin Radic <robin@radic.nl>
2016-10-26 21:43:17 +0200

    gulpfile update

--- gulpfile.ts
+++ gulpfile.ts
@@ -36,7 +36,7 @@ let c = {
 }
 
 gulp.task('clean', [ 'clean:src', 'clean:build' ])
-gulp.task('clean:build', () => gulp.src([ 'dist', 'dts', 'es', 'lib', 'umd', 'coverage', 'radic.util.*' ]).pipe(clean()))
+gulp.task('clean:build', () => gulp.src([ 'dist', 'dts', 'es', 'lib', 'umd', 'coverage' ]).pipe(clean()))
 gulp.task('clean:src', () => gulp.src([ '{src,spec}/*.{js,js.map}', '*.{js,js.map}' ]).pipe(clean()))
 
 //******************************************************************************
