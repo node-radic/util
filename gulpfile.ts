ed87f27f3db09f7455db991bf2b221232db63e28
Robin Radic <robin@radic.nl>
2017-08-23 03:29:02 +0200

    upd 2

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,6 @@
 import * as gulp from "gulp";
 import { WatchOptions } from "gulp";
 import * as _ from "lodash";
-import * as rollup from 'gulp-rollup'
 
 const
     pump        = require('pump'),
@@ -25,18 +24,17 @@ const c         = {
     umdModuleName: 'radic.util'
 }
 
-// import typedoc from 'gulp-typedoc';
-// typedoc({})
 const tsProject = {
-    es  : tsc.createProject("tsconfig.json", { module: "es2015",  declaration: false, typescript: ts }),
-    src : tsc.createProject("tsconfig.json", { typescript: ts, sourceMap: true }),
-    lib : tsc.createProject("tsconfig.json", { typescript: ts, declaration: false, target: 'es5' }),
-    dts : tsc.createProject("tsconfig.json", { typescript: ts, declaration: true, target: 'es5' }),
+    es  : tsc.createProject("tsconfig.json", { typescript: ts, module: "es2015", }),
+    src : tsc.createProject("tsconfig.json", { typescript: ts }),
+    lib : tsc.createProject("tsconfig.json", { typescript: ts, module: 'commonjs' }),
+    amd : tsc.createProject("tsconfig.json", { typescript: ts, module: 'amd' }),
+    dts : tsc.createProject("tsconfig.json", { typescript: ts, declaration: true, noResolve: false }),
     test: tsc.createProject("tsconfig.json", { target: "es6", sourceMap: true, typescript: ts })
 };
 gulp.task('clean', [ 'clean:src:js', 'clean:build', 'clean:docs' ], (cb) => pump([ gulp.src([ '.nyc_output', 'coverage' ]), clean() ]))
 gulp.task('clean:docs', (cb) => pump([ gulp.src([ 'docs', '.publish' ]), clean() ]))
-gulp.task('clean:build', (cb) => pump([ gulp.src([ 'lib', 'lib-es6', 'dts', 'coverage', '.publish', 'docs' ]), clean() ]));
+gulp.task('clean:build', (cb) => pump([ gulp.src([ 'lib', 'lib-es6','es', 'dts', 'coverage', '.publish', 'docs' ]), clean() ]));
 gulp.task('clean:watch', (cb) => pump([ gulp.src([ 'lib', 'dts' ]), clean() ]));
 
 gulp.task('clean:src:js', (cb) => pump([ gulp.src([ '{src,examples}/*.{js,js.map}', '*.{js,js.map}' ]), clean() ]));
@@ -44,10 +42,10 @@ gulp.task('clean:test:js', (cb) => pump([ gulp.src([ '{tests}/*.{js,js.map}', '*
 
 gulp.task('clean:dts:js', (cb) => pump([ gulp.src([ 'dts/**/*.js' ]), clean() ]))
 
-gulp.task("build:lib:es6", (cb) => pump([
+gulp.task("build:lib:es", (cb) => pump([
     gulp.src(c.src),
     tsProject.es(),
-    gulp.dest("lib-es6/")
+    gulp.dest("es/")
 ]))
 
 gulp.task("build:dts:ts", (cb) => pump([
@@ -68,7 +66,7 @@ gulp.task('build:src', (cb) => pump([
     gulp.dest("src/")
 ]))
 
-gulp.task("build:test", ['build:src'], (cb) => pump([
+gulp.task("build:test", [ 'build:src' ], (cb) => pump([
     gulp.src([ "tests/**/*.ts" ]),
     tsProject.test(),
     gulp.dest("tests/")
@@ -84,10 +82,10 @@ gulp.task("build:watch", (cb) => runSequence(
 gulp.task('watch', () => { gulp.watch(c.src, <WatchOptions>{ debounceDelay: 3000, interval: 3000 }, [ 'build:watch' ])})
 
 
-gulp.task('build:umd', [ 'build:lib:es6' ], () => pump([
-    gulp.src('lib-es6/**/*.js'),
+gulp.task('build:umd', [ 'build:lib:es' ], () => pump([
+    gulp.src('es/**/*.js'),
     rollup({
-        entry     : './lib-es6/index.js',
+        entry     : './es/index.js',
         format    : 'umd',
         moduleName: 'radic.util',
         globals   : { lodash: '_' },
@@ -109,13 +107,11 @@ gulp.task('build:umd:minify', [ 'build:umd' ], () => pump([
 ]))
 
 
-
 gulp.task("build", (cb) => runSequence(
     "clean",
-    [ "build:src", "build:lib", 'build:lib:es6', 'build:dts', 'build:umd', 'build:umd:minify' ],
+    [ "build:src", "build:lib", 'build:lib:es', 'build:dts', 'build:umd', 'build:umd:minify' ],
     "build:test", cb
 ));
-
 
 
 gulp.task("default", [ 'build' ])
